a8de3896b7b14baf7fd389416079670e50890c7a
Dmitry Alenichev <d.alenichev@corp.mail.ru>
2021-06-19 02:07:06 +0300

    more tests

--- whois_test.go
+++ whois_test.go
@@ -6,8 +6,9 @@ import (
 )
 
 const (
-	ianaWhois     = "whois.iana.org"
-	exampleDomain = "example.com"
+	ianaWhois  = "whois.iana.org"
+	exampleCom = "example.com"
+	rockersSu  = "rockers.su"
 )
 
 var exampleComOut = `% IANA WHOIS server
@@ -24,12 +25,23 @@ source:       IANA
 `
 
 func TestExampleCom(t *testing.T) {
-	out, err := MakeWhoisQuery(ianaWhois, exampleDomain)
+	out, err := MakeWhoisQuery(ianaWhois, exampleCom)
 	if err != nil {
 		t.Errorf("MakeWhoisQuery() error: %s", err)
 	}
 
 	if strings.Compare(exampleComOut, out) != 0 {
 		t.Errorf("output differs: %v %v", exampleComOut, out)
+	}
+}
+
+func TestRockersSu(t *testing.T) {
+	out, err := MakeWhoisQueryAll(rockersSu)
+	if err != nil {
+		t.Errorf("MakeWhoisQueryAll() error: %s", err)
+	}
+
+	if strings.Contains(out, "TCI") != true {
+		t.Errorf("output does not contains TCI: %v", out)
 	}
 }
